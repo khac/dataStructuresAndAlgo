4f3d8cafce99aed6ac9ebb6890ad69766e6521bf
Adit <adit@Adits-MacBook-Pro.local>
2022-06-26 00:34:08 -0700

    update: maximumPointsYouCanObtainFromCards

--- maximumPointsYouCanObtainFromCards.java
+++ maximumPointsYouCanObtainFromCards.java
@@ -1,9 +1,10 @@
 // 1423. Maximum Points You Can Obtain from Cards
 // https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
+// topics: dynamic programming, arrays
 
 import java.util.Arrays;
 
-class maxPmaximumPointsYouCanObtainFromCards {
+class maximumPointsYouCanObtainFromCards {
     public static void main (String[] args) {
         System.out.println(maxP(new int[]{1,2,3,4,5,6,1}, 3));
         System.out.println(maxP(new int[]{2,2,2}, 2));
