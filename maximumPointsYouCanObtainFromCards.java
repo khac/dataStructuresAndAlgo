d5bf557c2d42702573dfe1077356ba10551143f8
Adit Chawdhary <adit@Adits-MacBook-Pro.local>
2022-06-26 00:09:38 -0700

    dsAndAlgos: 1423. Maximum Points You Can Obtain from Cards

---
+++ maximumPointsYouCanObtainFromCards.java
@@ -0,0 +1,27 @@
+// 1423. Maximum Points You Can Obtain from Cards
+// https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
+
+import java.util.Arrays;
+
+class maximumPointsYouCanObtainFromCards {
+    public static void main (String[] args) {
+        System.out.println(maximumPointsYouCanObtainFromCards(new int[]{1,2,3,4,5,6,1}, 3));
+        System.out.println(maximumPointsYouCanObtainFromCards(new int[]{2,2,2}, 2));
+        System.out.println(maximumPointsYouCanObtainFromCards(new int[]{9,7,7,9,7,7,9}, 7));
+    }
+
+    public static int maximumPointsYouCanObtainFromCards(int[] cardPoints, int k) {
+        if(k == cardPoints.length) {
+            return Arrays.stream(cardPoints).reduce(0, (a, b) -> a+b);
+        }
+
+        int sumk = Arrays.stream(cardPoints, 0, k).reduce(0, (a, b) -> a+b);
+        int maxk = sumk;
+        for(int i=0; i<k; i++) {
+            sumk -= cardPoints[k-1-i];
+            sumk += cardPoints[cardPoints.length-1-i];
+            maxk = Math.max(sumk, maxk);
+        }
+        return maxk;
+    }
+}
